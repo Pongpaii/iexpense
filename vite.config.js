44cb11e7ea6441122217e6a565dfdf8b7bbee4c7
ppp081 <118096987+sStoowie@users.noreply.github.com>
2026-08-28 11:24:09 +0700

    feat: add iOS and PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,20 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            injectRegister: 'auto',
+            manifest: false,
+            workbox: {
+                cleanupOutdatedCaches: true,
+                clientsClaim: true,
+                skipWaiting: true,
+                navigateFallback: 'index.html',
+                globPatterns: ['**/*.{html,js,css,svg,png,ico,webmanifest}'],
+            },
+        }),
+    ],
 });
